7a0e24ebf3474040c16fc599c35cc895b1b43424
buddhamagnet <buddhamagnet@gmail.com>
2015-09-15 23:08:29 +0100

    add tests for driver registration

--- driver/driver_test.go
+++ driver/driver_test.go
@@ -7,3 +7,21 @@ func TestNew(t *testing.T) {
 		t.Error("no error although driver unknown")
 	}
 }
+
+func TestNewBash(t *testing.T) {
+	driver, err := New("bash://url")
+	if err != nil {
+		t.Error("error although bash driver known")
+	}
+	version, err := driver.Version()
+	if version != 0 {
+		t.Errorf("expected bash driver version to be 0, got %d\n", version)
+	}
+}
+
+func TestNewSqlite3(t *testing.T) {
+	_, err := New("sqlite3://test.db")
+	if err != nil {
+		t.Error("error although sqlite3 driver known")
+	}
+}
