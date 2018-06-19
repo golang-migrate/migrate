6070aa8281007929a35ff9a54e82ddc319bc92a8
Dale Hui <dhui@users.noreply.github.com>
2018-06-19 01:42:37 -0700

    Push latest image to docker hub

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -2,4 +2,4 @@
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin && \
 docker build --build-arg VERSION="$TRAVIS_TAG" . -t migrate/migrate:"$TRAVIS_TAG" && \
-docker push migrate/migrate:"$TRAVIS_TAG"
+docker push migrate/migrate:"$TRAVIS_TAG" && docker push migrate/migrate
