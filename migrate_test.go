6c02d65563d50e55b9a938cf10971c5cd97e3096
Dale Hui <dhui@users.noreply.github.com>
2018-09-14 14:12:07 -0700

    Update commented out imports to use v3

--- migrate_test.go
+++ migrate_test.go
@@ -104,7 +104,7 @@ func ExampleNewWithDatabaseInstance() {
 
 	// Create driver instance from db.
 	// Check each driver if it supports the WithInstance function.
-	// `import "github.com/golang-migrate/migrate/database/postgres"`
+	// `import "github.com/golang-migrate/migrate/v3/database/postgres"`
 	instance, err := dStub.WithInstance(db, &dStub.Config{})
 	if err != nil {
 		log.Fatal(err)
@@ -154,7 +154,7 @@ func ExampleNewWithSourceInstance() {
 
 	// Create driver instance from DummyInstance di.
 	// Check each driver if it support the WithInstance function.
-	// `import "github.com/golang-migrate/migrate/source/stub"`
+	// `import "github.com/golang-migrate/migrate/v3/source/stub"`
 	instance, err := sStub.WithInstance(di, &sStub.Config{})
 	if err != nil {
 		log.Fatal(err)
