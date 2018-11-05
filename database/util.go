16d63e3a760ede4e216f53105fce0df0f14037e8
Evgeniy Bastrykov <vporoshok@gmail.com>
2018-11-05 12:27:28 +0400

    Add support for multi-schema migrations in Postgres
    There is lock conflict on parallel migrations in different postgres
    schemas. To avoid this conflicts function GenerateAdvisoryLockId added
    variadic params to change lock id with schema name. Schema name taked
    with postgres CURRENT_SCHEMA function. Null byte used as separator
    between database and schema name, because any other symbol may be used
    in both of it.
    
    Closes #118

--- database/util.go
+++ database/util.go
@@ -1,6 +1,7 @@
 package database
 
 import (
+	"bytes"
 	"fmt"
 	"hash/crc32"
 )
@@ -8,8 +9,13 @@ import (
 const advisoryLockIdSalt uint = 1486364155
 
 // GenerateAdvisoryLockId inspired by rails migrations, see https://goo.gl/8o9bCT
-func GenerateAdvisoryLockId(databaseName string) (string, error) {
-	sum := crc32.ChecksumIEEE([]byte(databaseName))
+func GenerateAdvisoryLockId(databaseName string, additionalNames ...string) (string, error) {
+	buf := bytes.NewBufferString(databaseName)
+	for _, name := range additionalNames {
+		buf.WriteByte(0)
+		buf.WriteString(name)
+	}
+	sum := crc32.ChecksumIEEE(buf.Bytes())
 	sum = sum * uint32(advisoryLockIdSalt)
 	return fmt.Sprintf("%v", sum), nil
 }
--- database/util_test.go
+++ database/util_test.go
@@ -7,14 +7,21 @@ import (
 func TestGenerateAdvisoryLockId(t *testing.T) {
 	testcases := []struct {
 		dbname     string
+		schema     string
 		expectedID string // empty string signifies that an error is expected
 	}{
 		{dbname: "database_name", expectedID: "1764327054"},
+		{dbname: "database_name", schema: "schema_name_1", expectedID: "3244152297"},
+		{dbname: "database_name", schema: "schema_name_2", expectedID: "810103531"},
 	}
 
 	for _, tc := range testcases {
 		t.Run(tc.dbname, func(t *testing.T) {
-			if id, err := GenerateAdvisoryLockId("database_name"); err == nil {
+			names := []string{}
+			if len(tc.schema) > 0 {
+				names = append(names, tc.schema)
+			}
+			if id, err := GenerateAdvisoryLockId(tc.dbname, names...); err == nil {
 				if id != tc.expectedID {
 					t.Error("Generated incorrect ID:", id, "!=", tc.expectedID)
 				}
