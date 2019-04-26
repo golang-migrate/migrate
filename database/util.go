ae47e064dfae89fa545a18d51f5cb5fc48fb3fee
Kirill Muratov <kirill@ibclabs.com>
2019-04-26 16:13:39 +0300

    Fixed nolint comment

--- database/util.go
+++ database/util.go
@@ -9,6 +9,7 @@ import (
 const advisoryLockIDSalt uint = 1486364155
 
 // GenerateAdvisoryLockId inspired by rails migrations, see https://goo.gl/8o9bCT
+// nolint: golint
 func GenerateAdvisoryLockId(databaseName string, additionalNames ...string) (string, error) {
 	if len(additionalNames) > 0 {
 		databaseName = strings.Join(append(additionalNames, databaseName), "\x00")
