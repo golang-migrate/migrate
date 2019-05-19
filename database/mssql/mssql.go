c273636efe14ce8f47671ee1742d6994e1f7c6ec
nathan-c <nathan-c@hotmail.co.uk>
2019-05-19 14:08:10 +0100

    parse mssql errors

--- database/mssql/mssql.go
+++ database/mssql/mssql.go
@@ -206,6 +206,13 @@ func (ss *MSSQL) Run(migration io.Reader) error {
 	// run migration
 	query := string(migr[:])
 	if _, err := ss.conn.ExecContext(context.Background(), query); err != nil {
+		if msErr, ok := err.(*mssql.Error); ok {
+			message := fmt.Sprintf("migration failed: %s", msErr.Message)
+			if msErr.ProcName != "" {
+				message = fmt.Sprintf("%s (proc name %s)", msErr.Message, msErr.ProcName)
+			}
+			return database.Error{OrigErr: err, Err: message, Query: migr, Line: uint(msErr.LineNo)}
+		}
 		return database.Error{OrigErr: err, Err: "migration failed", Query: migr}
 	}
 
