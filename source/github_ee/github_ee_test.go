7aa8859c6891712d2d13d6c27dbb1980b174835d
Kasparas Galdikas <kasparas.galdikas@rakuten.com>
2019-06-14 13:27:22 +0100

    make linter happy

--- source/github_ee/github_ee_test.go
+++ source/github_ee/github_ee_test.go
@@ -21,7 +21,12 @@ func Test(t *testing.T) {
 
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("[]"))
+
+		_, err := w.Write([]byte("[]"))
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 	}))
 	defer ts.Close()
 
