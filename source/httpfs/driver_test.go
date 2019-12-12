7b6a813149c3c3c53a8918364359b964476d0ae4
Julius Kriukas <julius.kriukas@adv.bet>
2019-12-12 11:00:26 +0200

    Replace test messages with sub-test names

--- source/httpfs/driver_test.go
+++ source/httpfs/driver_test.go
@@ -9,82 +9,86 @@ import (
 
 func TestWithInstanceAndNew(t *testing.T) {
 	tests := []struct {
-		msg  string
+		name string
 		fs   http.FileSystem
 		path string
 		ok   bool
 	}{
 		{
-			msg: "valid dir, empty path",
-			fs:  http.Dir("testdata/sql"),
-			ok:  true,
+			name: "valid dir and empty path",
+			fs:   http.Dir("testdata/sql"),
+			ok:   true,
 		},
 		{
-			msg:  "valid dir, non-empty path",
+			name: "valid dir and non-empty path",
 			fs:   http.Dir("testdata"),
 			path: "sql",
 			ok:   true,
 		},
 		{
-			msg: "invalid dir",
-			fs:  http.Dir("does-not-exist"),
+			name: "invalid dir",
+			fs:   http.Dir("does-not-exist"),
 		},
 		{
-			msg: "file instead of dir",
-			fs:  http.Dir("testdata/sql/1_foobar.up.sql"),
+			name: "file instead of dir",
+			fs:   http.Dir("testdata/sql/1_foobar.up.sql"),
 		},
 		{
-			msg: "dir with duplicates",
-			fs:  http.Dir("testdata/duplicates"),
+			name: "dir with duplicates",
+			fs:   http.Dir("testdata/duplicates"),
 		},
 	}
 
 	for _, test := range tests {
-		d, err := WithInstance(test.fs, test.path)
-		if test.ok {
-			if err != nil {
-				t.Errorf("%s, WithInstance() returned error %s", test.msg, err)
+		t.Run("WithInstance "+test.name, func(t *testing.T) {
+			d, err := WithInstance(test.fs, test.path)
+			if test.ok {
+				if err != nil {
+					t.Errorf("WithInstance() returned error %s", err)
+				}
+				st.Test(t, d)
+				if err = d.Close(); err != nil {
+					t.Errorf("WithInstance().Close() returned error %s", err)
+				}
 			}
-			st.Test(t, d)
-			if err = d.Close(); err != nil {
-				t.Errorf("%s, WithInstance().Close() returned error %s", test.msg, err)
+			if !test.ok {
+				if err == nil {
+					t.Errorf("WithInstance() expected error but did not get one")
+				}
 			}
-		}
-		if !test.ok {
-			if err == nil {
-				t.Errorf("%s, WithInstance() expected error but did not get one", test.msg)
-			}
-		}
+		})
 	}
 
 	for _, test := range tests {
-		d := New(test.fs, test.path)
-		if test.ok {
-			st.Test(t, d)
-		}
-		if !test.ok {
-			if _, err := d.Open(""); err == nil {
-				t.Errorf("%s, New().Open() expected error but did not get one", test.msg)
+		t.Run("New "+test.name, func(t *testing.T) {
+			d := New(test.fs, test.path)
+			if test.ok {
+				st.Test(t, d)
 			}
-			if err := d.Close(); err == nil {
-				t.Errorf("%s, New().Close() expected error but did not get one", test.msg)
+			if !test.ok {
+				if _, err := d.Open(""); err == nil {
+					t.Errorf("Open() expected error but did not get one")
+				}
+				if err := d.Close(); err == nil {
+					t.Errorf("Close() expected error but did not get one")
+				}
+				if _, err := d.First(); err == nil {
+					t.Errorf("First() expected error but did not get one")
+				}
+				if _, err := d.Prev(0); err == nil {
+					t.Errorf("Prev() expected error but did not get one")
+				}
+				if _, err := d.Next(0); err == nil {
+					t.Errorf("Next() expected error but did not get one")
+				}
+				if _, _, err := d.ReadUp(0); err == nil {
+					t.Errorf("ReadUp() expected error but did not get one")
+				}
+				if _, _, err := d.ReadDown(0); err == nil {
+					t.Errorf("ReadDown() expected error but did not get one")
+				}
 			}
-			if _, err := d.First(); err == nil {
-				t.Errorf("%s, New().First() expected error but did not get one", test.msg)
-			}
-			if _, err := d.Prev(0); err == nil {
-				t.Errorf("%s, New().Prev() expected error but did not get one", test.msg)
-			}
-			if _, err := d.Next(0); err == nil {
-				t.Errorf("%s, New().Next() expected error but did not get one", test.msg)
-			}
-			if _, _, err := d.ReadUp(0); err == nil {
-				t.Errorf("%s, New().ReadUp() expected error but did not get one", test.msg)
-			}
-			if _, _, err := d.ReadDown(0); err == nil {
-				t.Errorf("%s, New().ReadDown() expected error but did not get one", test.msg)
-			}
-		}
+		})
 	}
 
 }
