46206b733054a048153c2ede56d44c5489713217
Julius Kriukas <julius@kriukas.lt>
2019-12-13 02:09:14 +0200

    Split Migrator and Driver tests

--- source/httpfs/driver_test.go
+++ source/httpfs/driver_test.go
@@ -15,7 +15,7 @@ type driver struct{ httpfs.Migrator }
 
 func (d *driver) Open(url string) (source.Driver, error) { return nil, errors.New("X") }
 
-func TestWithInstanceAndNew(t *testing.T) {
+func TestMigratorInit(t *testing.T) {
 	tests := []struct {
 		name string
 		fs   http.FileSystem
@@ -48,55 +48,56 @@ func TestWithInstanceAndNew(t *testing.T) {
 	}
 
 	for _, test := range tests {
-		t.Run("Init "+test.name, func(t *testing.T) {
+		t.Run(test.name, func(t *testing.T) {
 			var d driver
 			err := d.Init(test.fs, test.path)
 			if test.ok {
 				if err != nil {
-					t.Errorf("WithInstance() returned error %s", err)
+					t.Errorf("Init() returned error %s", err)
 				}
 				st.Test(t, &d)
 				if err = d.Close(); err != nil {
-					t.Errorf("WithInstance().Close() returned error %s", err)
+					t.Errorf("Init().Close() returned error %s", err)
 				}
 			} else {
 				if err == nil {
-					t.Errorf("WithInstance() expected error but did not get one")
+					t.Errorf("Init() expected error but did not get one")
 				}
 			}
 		})
 	}
 
-	for _, test := range tests {
-		t.Run("New "+test.name, func(t *testing.T) {
-			d := httpfs.New(test.fs, test.path)
-			if test.ok {
-				st.Test(t, d)
-			} else {
-				if _, err := d.Open(""); err == nil {
-					t.Errorf("Open() expected error but did not get one")
-				}
-				if err := d.Close(); err == nil {
-					t.Errorf("Close() expected error but did not get one")
-				}
-				if _, err := d.First(); err == nil {
-					t.Errorf("First() expected error but did not get one")
-				}
-				if _, err := d.Prev(0); err == nil {
-					t.Errorf("Prev() expected error but did not get one")
-				}
-				if _, err := d.Next(0); err == nil {
-					t.Errorf("Next() expected error but did not get one")
-				}
-				if _, _, err := d.ReadUp(0); err == nil {
-					t.Errorf("ReadUp() expected error but did not get one")
-				}
-				if _, _, err := d.ReadDown(0); err == nil {
-					t.Errorf("ReadDown() expected error but did not get one")
-				}
-			}
-		})
-	}
+}
+
+func TestNew(t *testing.T) {
+	t.Run("OK", func(t *testing.T) {
+		d := httpfs.New(http.Dir("testdata"), "sql")
+		st.Test(t, d)
+	})
+	t.Run("Errors", func(t *testing.T) {
+		d := httpfs.New(http.Dir("does-not-exist"), "")
+		if _, err := d.Open(""); err == nil {
+			t.Errorf("Open() expected error but did not get one")
+		}
+		if err := d.Close(); err == nil {
+			t.Errorf("Close() expected error but did not get one")
+		}
+		if _, err := d.First(); err == nil {
+			t.Errorf("First() expected error but did not get one")
+		}
+		if _, err := d.Prev(0); err == nil {
+			t.Errorf("Prev() expected error but did not get one")
+		}
+		if _, err := d.Next(0); err == nil {
+			t.Errorf("Next() expected error but did not get one")
+		}
+		if _, _, err := d.ReadUp(0); err == nil {
+			t.Errorf("ReadUp() expected error but did not get one")
+		}
+		if _, _, err := d.ReadDown(0); err == nil {
+			t.Errorf("ReadDown() expected error but did not get one")
+		}
+	})
 }
 
 func TestFirstWithNoMigrations(t *testing.T) {
