bfa4bdf7a060ef5c09e57be0c2a4d0c684ca5d47
Julius Kriukas <julius@kriukas.lt>
2019-12-16 10:11:31 +0200

    Split New() tests

--- source/httpfs/driver_test.go
+++ source/httpfs/driver_test.go
@@ -8,33 +8,32 @@ import (
 	st "github.com/golang-migrate/migrate/v4/source/testing"
 )
 
-func TestNew(t *testing.T) {
-	t.Run("OK", func(t *testing.T) {
-		d := httpfs.New(http.Dir("testdata"), "sql")
-		st.Test(t, d)
-	})
-	t.Run("Errors", func(t *testing.T) {
-		d := httpfs.New(http.Dir("does-not-exist"), "")
-		if _, err := d.Open(""); err == nil {
-			t.Errorf("Open() expected error but did not get one")
-		}
-		if err := d.Close(); err == nil {
-			t.Errorf("Close() expected error but did not get one")
-		}
-		if _, err := d.First(); err == nil {
-			t.Errorf("First() expected error but did not get one")
-		}
-		if _, err := d.Prev(0); err == nil {
-			t.Errorf("Prev() expected error but did not get one")
-		}
-		if _, err := d.Next(0); err == nil {
-			t.Errorf("Next() expected error but did not get one")
-		}
-		if _, _, err := d.ReadUp(0); err == nil {
-			t.Errorf("ReadUp() expected error but did not get one")
-		}
-		if _, _, err := d.ReadDown(0); err == nil {
-			t.Errorf("ReadDown() expected error but did not get one")
-		}
-	})
+func TestNewOK(t *testing.T) {
+	d := httpfs.New(http.Dir("testdata"), "sql")
+	st.Test(t, d)
+}
+
+func TestNewErrors(t *testing.T) {
+	d := httpfs.New(http.Dir("does-not-exist"), "")
+	if _, err := d.Open(""); err == nil {
+		t.Errorf("Open() expected error but did not get one")
+	}
+	if err := d.Close(); err == nil {
+		t.Errorf("Close() expected error but did not get one")
+	}
+	if _, err := d.First(); err == nil {
+		t.Errorf("First() expected error but did not get one")
+	}
+	if _, err := d.Prev(0); err == nil {
+		t.Errorf("Prev() expected error but did not get one")
+	}
+	if _, err := d.Next(0); err == nil {
+		t.Errorf("Next() expected error but did not get one")
+	}
+	if _, _, err := d.ReadUp(0); err == nil {
+		t.Errorf("ReadUp() expected error but did not get one")
+	}
+	if _, _, err := d.ReadDown(0); err == nil {
+		t.Errorf("ReadDown() expected error but did not get one")
+	}
 }
