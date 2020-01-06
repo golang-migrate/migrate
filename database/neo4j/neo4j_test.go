e67feb18a62805a5ade11c63876f8cd044640ed5
Mantas Vidutis <mantas.a.vidutis@gmail.com>
2020-01-06 17:19:33 +0530

    add ready function for docker start

--- database/neo4j/neo4j_test.go
+++ database/neo4j/neo4j_test.go
@@ -1,38 +1,67 @@
 package neo4j
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"testing"
 
 	"github.com/dhui/dktest"
-	"github.com/golang-migrate/migrate/v4/dktesting"
+	"github.com/neo4j/neo4j-go-driver/neo4j"
 
 	dt "github.com/golang-migrate/migrate/v4/database/testing"
-
+	"github.com/golang-migrate/migrate/v4/dktesting"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
 var (
-	opts = dktest.Options{PortRequired: true, ReadyFunc: nil}
+	opts = dktest.Options{PortRequired: true, ReadyFunc: isReady}
 	specs = []dktesting.ContainerSpec{
 		{ImageName: "neo4j:3.5", Options: opts},
-		{ImageName: "neo4j:3.5-enterprise", Options: opts},
+		//{ImageName: "neo4j:3.5-enterprise", Options: opts},
 	}
 )
 
 func neoConnectionString(host, port string) string {
-	return fmt.Sprintf("neo4j://neo4j:neo4j@%s:%s", host, port)
+	return fmt.Sprintf("bolt://neo4j:neo4j@%s:%s", host, port)
+}
+
+func isReady(ctx context.Context, c dktest.ContainerInfo) bool {
+	ip, port, err := c.Port(7687)
+	if err != nil {
+		return false
+	}
+
+	driver, err := neo4j.NewDriver(neoConnectionString(ip, port), neo4j.BasicAuth("neo4j", "neo4j", ""))
+	if err != nil {
+		return false
+	}
+	defer func() {
+		if err := driver.Close(); err != nil {
+			log.Println("close error:", err)
+		}
+	}()
+	session, err := driver.Session(neo4j.AccessModeRead)
+	if err != nil {
+		return false
+	}
+	_, err = session.Run("RETURN 1", nil)
+	if err != nil {
+		return false
+	}
+
+	return true
 }
 
 func Test(t *testing.T) {
 	dktesting.ParallelTest(t, specs, func(t *testing.T, c dktest.ContainerInfo) {
-		ip, port, err := c.FirstPort()
+		ip, port, err := c.Port(7687)
 		if err != nil {
 			t.Fatal(err)
 		}
 
-		addr := neoConnectionString(ip, port)
 		n := &Neo4J{}
-		d, err := n.Open(addr)
+		d, err := n.Open(neoConnectionString(ip, port))
 		if err != nil {
 			t.Fatal(err)
 		}
