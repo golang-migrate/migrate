dceedfe4af356511c97b240f479780304ec5ca1d
Xianchao.Yu <xianchao.yu@target-energysolutions.com>
2020-02-18 23:51:27 +0000

    add dk test cases

--- database/oracle/oracle_test.go
+++ database/oracle/oracle_test.go
@@ -4,23 +4,60 @@ import (
 	"bytes"
 	"context"
 	"database/sql"
+	sqldriver "database/sql/driver"
+	"fmt"
+	"io"
+	"log"
 	"os"
 	"sync"
 	"testing"
+	"time"
 
+	"github.com/dhui/dktest"
 	"github.com/golang-migrate/migrate/v4"
 	dt "github.com/golang-migrate/migrate/v4/database/testing"
+	"github.com/golang-migrate/migrate/v4/dktesting"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/stretchr/testify/require"
 )
 
-func oracleDsn(t *testing.T) string {
+var (
+	opts = dktest.Options{PortRequired: true, ReadyFunc: isReady, ReadyTimeout: time.Minute}
+	// Supported versions: https://www.postgresql.org/support/versioning/
+	specs = []dktesting.ContainerSpec{
+		{ImageName: "maxnilz/oracle-xe:18c", Options: opts},
+	}
+)
+
+type dsnFunc func(t *testing.T, args ...interface{}) string
+
+func oracleEnvDsn(t *testing.T, _ ...interface{}) string {
 	//E.g: oci8://user/password@localhost:1521/ORCLPDB1
 	dsn := os.Getenv("MIGRATE_TEST_ORACLE_DSN")
 	if dsn == "" {
-		t.Skip("ORACLE_DSN not found, skip the test case")
+		t.Skip("MIGRATE_TEST_ORACLE_DSN not found, skip the test case")
 	}
 	return dsn
+}
+
+func isDKHonored(t *testing.T) {
+	s := os.Getenv("MIGRATE_TEST_ENABLE_ORACLE_CONTAINER")
+	if s != "true" {
+		t.Skip("MIGRATE_TEST_ENABLE_ORACLE_CONTAINER not found, skip the dk test case")
+	}
+}
+
+func oracleDKDsn(t *testing.T, args ...interface{}) string {
+	c := args[0].(dktest.ContainerInfo)
+	ip, port, err := c.FirstPort()
+	if err != nil {
+		t.Fatal(err)
+	}
+	return oracleConnectionString(ip, port)
+}
+
+func oracleConnectionString(host, port string) string {
+	return fmt.Sprintf("oracle://oracle:oracle@%s:%s/XEPDB1", host, port)
 }
 
 func TestParseStatements(t *testing.T) {
@@ -103,8 +140,79 @@ ALTER TABLE USERS ADD CITY varchar(100);
 }
 
 func TestOpen(t *testing.T) {
+	testOpen(t, oracleEnvDsn)
+}
+
+func TestMigrate(t *testing.T) {
+	testMigrate(t, oracleEnvDsn)
+}
+
+func TestLockWorks(t *testing.T) {
+	testLockWorks(t, oracleEnvDsn)
+}
+
+func TestWithInstanceConcurrent(t *testing.T) {
+	testWithInstanceConcurrent(t, oracleEnvDsn)
+}
+
+func isReady(ctx context.Context, c dktest.ContainerInfo) bool {
+	ip, port, err := c.FirstPort()
+	if err != nil {
+		return false
+	}
+	db, err := sql.Open("godror", oracleConnectionString(ip, port))
+	if err != nil {
+		return false
+	}
+	defer func() {
+		if err := db.Close(); err != nil {
+			log.Println("close error:", err)
+		}
+	}()
+	if err = db.PingContext(ctx); err != nil {
+		switch err {
+		case sqldriver.ErrBadConn, io.EOF:
+			return false
+		default:
+			log.Println(err)
+		}
+		return false
+	}
+
+	return true
+}
+
+func TestOpenWithDK(t *testing.T) {
+	isDKHonored(t)
+	dktesting.ParallelTest(t, specs, func(t *testing.T, c dktest.ContainerInfo) {
+		testOpen(t, oracleDKDsn, c)
+	})
+}
+
+func TestMigrateWithDK(t *testing.T) {
+	isDKHonored(t)
+	dktesting.ParallelTest(t, specs, func(t *testing.T, c dktest.ContainerInfo) {
+		testMigrate(t, oracleDKDsn, c)
+	})
+}
+
+func TestLockWorksWithDK(t *testing.T) {
+	isDKHonored(t)
+	dktesting.ParallelTest(t, specs, func(t *testing.T, c dktest.ContainerInfo) {
+		testLockWorks(t, oracleDKDsn, c)
+	})
+}
+
+func TestWithInstanceConcurrentWithDK(t *testing.T) {
+	isDKHonored(t)
+	dktesting.ParallelTest(t, specs, func(t *testing.T, c dktest.ContainerInfo) {
+		testWithInstanceConcurrent(t, oracleDKDsn, c)
+	})
+}
+
+func testOpen(t *testing.T, oracleDsnFunc dsnFunc, args ...interface{}) {
 	ora := &Oracle{}
-	d, err := ora.Open(oracleDsn(t))
+	d, err := ora.Open(oracleDsnFunc(t, args...))
 	require.Nil(t, err)
 	require.NotNil(t, d)
 	defer func() {
@@ -123,9 +231,9 @@ func TestOpen(t *testing.T) {
 	dt.Test(t, d, []byte(`BEGIN DBMS_OUTPUT.PUT_LINE('hello'); END;`))
 }
 
-func TestMigrate(t *testing.T) {
+func testMigrate(t *testing.T, oracleDsnFunc dsnFunc, args ...interface{}) {
 	p := &Oracle{}
-	d, err := p.Open(oracleDsn(t))
+	d, err := p.Open(oracleDsnFunc(t, args...))
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -141,10 +249,9 @@ func TestMigrate(t *testing.T) {
 	dt.TestMigrate(t, m)
 }
 
-func TestLockWorks(t *testing.T) {
-	t.Parallel()
+func testLockWorks(t *testing.T, oracleDsnFunc dsnFunc, args ...interface{}) {
 	p := &Oracle{}
-	d, err := p.Open(oracleDsn(t))
+	d, err := p.Open(oracleDsnFunc(t, args...))
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -179,7 +286,7 @@ func TestLockWorks(t *testing.T) {
 	}
 }
 
-func TestWithInstance_Concurrent(t *testing.T) {
+func testWithInstanceConcurrent(t *testing.T, oracleDsnFunc dsnFunc, args ...interface{}) {
 	// The number of concurrent processes running WithInstance
 	const concurrency = 30
 
@@ -187,7 +294,7 @@ func TestWithInstance_Concurrent(t *testing.T) {
 	// actually a connection pool, and so, each of the below go
 	// routines will have a high probability of using a separate
 	// connection, which is something we want to exercise.
-	db, err := sql.Open("godror", oracleDsn(t))
+	db, err := sql.Open("godror", oracleDsnFunc(t, args...))
 	if err != nil {
 		t.Fatal(err)
 	}
