663520e7703dd00ea5c4876bbe48fcbac5956f1e
Hannes Gustafsson <hnnsgstfssn@gmail.com>
2020-04-18 00:22:34 +0100

    Add pkger source driver support
    As go-bindata has been abandoned [1] there are open requests, #116, for
    alternative sources with similar functionality. The Buffalo project [2]
    created packr and recently pkger [3] was announced [4] with the
    intention to supersede packr.
    
    This change adds support for using pkger as a source.
    
    The implementation relies on httpfs.PartialDriver for pretty much all
    functionality.
    
    [1] https://github.com/jteeuwen/go-bindata/issues/5
    [2] https://gobuffalo.io/
    [3] https://github.com/markbates/pkger
    [4] https://blog.gobuffalo.io/introducing-pkger-static-file-embedding-in-go-1ce76dc79c65

---
+++ source/pkger/pkger.go
@@ -0,0 +1,67 @@
+package pkger
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/golang-migrate/migrate/v4/source"
+	"github.com/golang-migrate/migrate/v4/source/httpfs"
+	"github.com/markbates/pkger/pkging"
+)
+
+func init() {
+	source.Register("pkger", &driver{})
+}
+
+// Instance is an implementation of http.FileSystem backed by an instance of
+// pkging.Pkger.
+type Instance struct {
+	pkging.Pkger
+
+	// Path is the relative path location of the migrations. It is passed to
+	// httpfs.PartialDriver.Init. If unset "/" is used as all paths are
+	// absolute.
+	Path string
+}
+
+// Open implements http.FileSystem.
+func (p *Instance) Open(name string) (http.File, error) {
+	f, err := p.Pkger.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	return f.(http.File), nil
+}
+
+type driver struct {
+	httpfs.PartialDriver
+}
+
+// Open implements source.Driver. NOT IMPLEMENTED.
+func (d *driver) Open(url string) (source.Driver, error) {
+	return nil, fmt.Errorf("not yet implemented")
+}
+
+// WithInstance returns a source.Driver that is backed by an Instance.
+func WithInstance(instance interface{}) (source.Driver, error) {
+	if _, ok := instance.(*Instance); !ok {
+		return nil, fmt.Errorf("expects *Instance")
+	}
+
+	p := instance.(*Instance)
+
+	if p.Path == "" {
+		p.Path = "/"
+	}
+
+	var fs http.FileSystem
+	var ds driver
+
+	fs = p
+
+	if err := ds.Init(fs, p.Path); err != nil {
+		return nil, fmt.Errorf("failed to init: %w", err)
+	}
+
+	return &ds, nil
+}
---
+++ source/pkger/pkger_test.go
@@ -0,0 +1,70 @@
+package pkger
+
+import (
+	"log"
+	"testing"
+
+	"github.com/gobuffalo/here"
+	st "github.com/golang-migrate/migrate/v4/source/testing"
+	"github.com/markbates/pkger/pkging"
+	"github.com/markbates/pkger/pkging/mem"
+)
+
+func Test(t *testing.T) {
+	i := testInstance(t)
+
+	d, err := WithInstance(i)
+	if err != nil {
+		t.Fatal(err)
+	}
+	st.Test(t, d)
+}
+
+func TestWithInstance(t *testing.T) {
+	i := testInstance(t)
+
+	_, err := WithInstance(i)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestOpen(t *testing.T) {
+	d := &driver{}
+	_, err := d.Open("")
+	if err == nil {
+		t.Fatal("expected err, because it's not implemented yet")
+	}
+}
+
+func testInstance(t *testing.T) *Instance {
+	info, err := here.New().Current()
+	if err != nil {
+		t.Fatalf("failed to get the current here.Info: %v\n", err)
+	}
+
+	pkg, err := mem.New(info)
+	if err != nil {
+		log.Fatalf("failed to create an in-memory pkging.Pkger: %v\n", err)
+	}
+
+	createMigrationFile(t, pkg, "/1_foobar.up.sql")
+	createMigrationFile(t, pkg, "/1_foobar.down.sql")
+	createMigrationFile(t, pkg, "/3_foobar.up.sql")
+	createMigrationFile(t, pkg, "/4_foobar.up.sql")
+	createMigrationFile(t, pkg, "/4_foobar.down.sql")
+	createMigrationFile(t, pkg, "/5_foobar.down.sql")
+	createMigrationFile(t, pkg, "/7_foobar.up.sql")
+	createMigrationFile(t, pkg, "/7_foobar.down.sql")
+
+	return &Instance{
+		Pkger: pkg,
+	}
+}
+
+func createMigrationFile(t *testing.T, pkg pkging.Pkger, m string) {
+	_, err := pkg.Create(m)
+	if err != nil {
+		t.Fatalf("failed to package migration file %q: %v\n", m, err)
+	}
+}
