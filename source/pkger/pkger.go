9afbbbfd89657769468b7fa5039a2fdead66e4d4
Hannes Gustafsson <hnnsgstfssn@gmail.com>
2020-04-19 23:21:33 +0100

    pkger: refactor and add access to global pkging.Pkger instance

--- source/pkger/pkger.go
+++ source/pkger/pkger.go
@@ -3,59 +3,81 @@ package pkger
 import (
 	"fmt"
 	"net/http"
+	stdurl "net/url"
 
 	"github.com/golang-migrate/migrate/v4/source"
 	"github.com/golang-migrate/migrate/v4/source/httpfs"
+	"github.com/markbates/pkger"
 	"github.com/markbates/pkger/pkging"
 )
 
 func init() {
-	source.Register("pkger", &driver{})
+	source.Register("pkger", &Pkger{})
 }
 
-// Pkger is an implementation of http.FileSystem backed by an instance of
+// Pkger is a source.Driver that reads migrations from instances of
 // pkging.Pkger.
 type Pkger struct {
-	pkging.Pkger
-
-	// Path is the relative path location of the migrations. It is passed to
-	// httpfs.PartialDriver.Init. If unset "/" is used as all paths are
-	// absolute.
-	Path string
-}
-
-// Open implements http.FileSystem.
-func (p *Pkger) Open(name string) (http.File, error) {
-	f, err := p.Pkger.Open(name)
-	if err != nil {
-		return nil, err
-	}
-	return f.(http.File), nil
-}
-
-type driver struct {
 	httpfs.PartialDriver
 }
 
-// Open implements source.Driver. NOT IMPLEMENTED.
-func (d *driver) Open(url string) (source.Driver, error) {
-	return nil, fmt.Errorf("not yet implemented")
+// Open implements source.Driver. The path component of url will be used as the
+// relative location of migrations. The returned driver will use the package
+// scoped pkger.Open to access migrations.  The relative root and any
+// migrations must be added to the global pkger.Pkger instance by calling
+// pkger.Apply. Refer to Pkger documentation for more information.
+func (p *Pkger) Open(url string) (source.Driver, error) {
+	u, err := stdurl.Parse(url)
+	if err != nil {
+		return nil, err
+	}
+
+	// wrap pkger to implement http.FileSystem.
+	fs := fsFunc(func(name string) (http.File, error) {
+		f, err := pkger.Open(name)
+		if err != nil {
+			return nil, err
+		}
+		return f.(http.File), nil
+	})
+
+	if err := p.Init(fs, u.Path); err != nil {
+		return nil, fmt.Errorf("failed to init driver with relative path %q: %w", u.Path, err)
+	}
+
+	return p, nil
 }
 
-// WithInstance returns a source.Driver that is backed by an instance of Pkger.
-func WithInstance(instance *Pkger) (source.Driver, error) {
-	if instance.Path == "" {
-		instance.Path = "/"
+// WithInstance returns a source.Driver that is backed by an instance of
+// pkging.Pkger. The relative location of migrations is indicated by path. The
+// path must exist on the pkging.Pkger instance for the driver to initialize
+// successfully.
+func WithInstance(instance pkging.Pkger, path string) (source.Driver, error) {
+	if instance == nil {
+		return nil, fmt.Errorf("expected instance of pkging.Pkger")
 	}
 
-	var fs http.FileSystem
-	var ds driver
+	// wrap pkger to implement http.FileSystem.
+	fs := fsFunc(func(name string) (http.File, error) {
+		f, err := instance.Open(name)
+		if err != nil {
+			return nil, err
+		}
+		return f.(http.File), nil
+	})
 
-	fs = instance
+	var p Pkger
 
-	if err := ds.Init(fs, instance.Path); err != nil {
-		return nil, fmt.Errorf("failed to init: %w", err)
+	if err := p.Init(fs, path); err != nil {
+		return nil, fmt.Errorf("failed to init driver with relative path %q: %w", path, err)
 	}
 
-	return &ds, nil
+	return &p, nil
+}
+
+type fsFunc func(name string) (http.File, error)
+
+// Open implements http.FileSystem.
+func (f fsFunc) Open(name string) (http.File, error) {
+	return f(name)
 }
--- source/pkger/pkger_test.go
+++ source/pkger/pkger_test.go
@@ -1,70 +1,197 @@
 package pkger
 
 import (
-	"log"
+	"errors"
+	"os"
 	"testing"
 
 	"github.com/gobuffalo/here"
 	st "github.com/golang-migrate/migrate/v4/source/testing"
+	"github.com/markbates/pkger"
 	"github.com/markbates/pkger/pkging"
 	"github.com/markbates/pkger/pkging/mem"
 )
 
 func Test(t *testing.T) {
-	i := testInstance(t)
+	t.Run("WithInstance", func(t *testing.T) {
+		i := testInstance(t)
 
-	d, err := WithInstance(i)
-	if err != nil {
-		t.Fatal(err)
-	}
-	st.Test(t, d)
+		createPkgerFile(t, i, "/1_foobar.up.sql")
+		createPkgerFile(t, i, "/1_foobar.down.sql")
+		createPkgerFile(t, i, "/3_foobar.up.sql")
+		createPkgerFile(t, i, "/4_foobar.up.sql")
+		createPkgerFile(t, i, "/4_foobar.down.sql")
+		createPkgerFile(t, i, "/5_foobar.down.sql")
+		createPkgerFile(t, i, "/7_foobar.up.sql")
+		createPkgerFile(t, i, "/7_foobar.down.sql")
+
+		d, err := WithInstance(i, "/")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		st.Test(t, d)
+	})
+
+	t.Run("Open", func(t *testing.T) {
+		i := testInstance(t)
+
+		createPkgerFile(t, i, "/1_foobar.up.sql")
+		createPkgerFile(t, i, "/1_foobar.down.sql")
+		createPkgerFile(t, i, "/3_foobar.up.sql")
+		createPkgerFile(t, i, "/4_foobar.up.sql")
+		createPkgerFile(t, i, "/4_foobar.down.sql")
+		createPkgerFile(t, i, "/5_foobar.down.sql")
+		createPkgerFile(t, i, "/7_foobar.up.sql")
+		createPkgerFile(t, i, "/7_foobar.down.sql")
+
+		registerPackageLevelInstance(t, i)
+
+		d, err := (&Pkger{}).Open("pkger:///")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		st.Test(t, d)
+	})
+
 }
 
 func TestWithInstance(t *testing.T) {
-	i := testInstance(t)
+	t.Run("Subdir", func(t *testing.T) {
+		i := testInstance(t)
 
-	_, err := WithInstance(i)
+		// Make sure the relative root exists so that httpfs.PartialDriver can
+		// initialize.
+		createPkgerSubdir(t, i, "/subdir")
+
+		_, err := WithInstance(i, "/subdir")
+		if err != nil {
+			t.Fatal("")
+		}
+	})
+
+	t.Run("NilInstance", func(t *testing.T) {
+		_, err := WithInstance(nil, "")
+		if err == nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("FailInit", func(t *testing.T) {
+		i := testInstance(t)
+
+		_, err := WithInstance(i, "/fail")
+		if err == nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("FailWithoutMigrations", func(t *testing.T) {
+		i := testInstance(t)
+
+		createPkgerSubdir(t, i, "/")
+
+		d, err := WithInstance(i, "/")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if _, err := d.First(); errors.Is(err, os.ErrNotExist) {
+			t.Log(err)
+			t.Fatal(err)
+		}
+
+	})
+}
+
+func TestOpen(t *testing.T) {
+
+	t.Run("InvalidURL", func(t *testing.T) {
+		_, err := (&Pkger{}).Open(":///")
+		if err == nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("Root", func(t *testing.T) {
+		_, err := (&Pkger{}).Open("pkger:///")
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+
+	t.Run("FailInit", func(t *testing.T) {
+		_, err := (&Pkger{}).Open("pkger:///subdir")
+		if err == nil {
+			t.Fatal(err)
+		}
+	})
+
+	i := testInstance(t)
+	createPkgerSubdir(t, i, "/subdir")
+
+	// Note that this registers the instance globally so anything run after
+	// this will have access to everything container in the registered
+	// instance.
+	registerPackageLevelInstance(t, i)
+
+	t.Run("Subdir", func(t *testing.T) {
+		_, err := (&Pkger{}).Open("pkger:///subdir")
+		if err != nil {
+			t.Fatal(err)
+		}
+	})
+}
+
+func TestClose(t *testing.T) {
+	d, err := (&Pkger{}).Open("pkger:///")
 	if err != nil {
+		t.Fatal(err)
+	}
+	if err := d.Close(); err != nil {
 		t.Fatal(err)
 	}
 }
 
-func TestOpen(t *testing.T) {
-	d := &driver{}
-	_, err := d.Open("")
-	if err == nil {
-		t.Fatal("expected err, because it's not implemented yet")
+func registerPackageLevelInstance(t *testing.T, pkg pkging.Pkger) {
+	if err := pkger.Apply(pkg, nil); err != nil {
+		t.Fatalf("failed to register pkger instance: %v\n", err)
 	}
 }
 
-func testInstance(t *testing.T) *Pkger {
+func testInstance(t *testing.T) pkging.Pkger {
+	pkg, err := inMemoryPkger()
+	if err != nil {
+		t.Fatalf("failed to create an  pkging.Pkger instance: %v\n", err)
+	}
+
+	return pkg
+}
+
+func createPkgerSubdir(t *testing.T, pkg pkging.Pkger, subdir string) {
+	if err := pkg.MkdirAll(subdir, os.ModePerm); err != nil {
+		t.Fatalf("failed to create pkger subdir %q: %v\n", subdir, err)
+	}
+}
+
+func createPkgerFile(t *testing.T, pkg pkging.Pkger, name string) {
+	_, err := pkg.Create(name)
+	if err != nil {
+		t.Fatalf("failed to create pkger file %q: %v\n", name, err)
+	}
+}
+
+func inMemoryPkger() (*mem.Pkger, error) {
 	info, err := here.New().Current()
 	if err != nil {
-		t.Fatalf("failed to get the current here.Info: %v\n", err)
+		return nil, err
 	}
 
 	pkg, err := mem.New(info)
 	if err != nil {
-		log.Fatalf("failed to create an in-memory pkging.Pkger: %v\n", err)
+		return nil, err
 	}
 
-	createMigrationFile(t, pkg, "/1_foobar.up.sql")
-	createMigrationFile(t, pkg, "/1_foobar.down.sql")
-	createMigrationFile(t, pkg, "/3_foobar.up.sql")
-	createMigrationFile(t, pkg, "/4_foobar.up.sql")
-	createMigrationFile(t, pkg, "/4_foobar.down.sql")
-	createMigrationFile(t, pkg, "/5_foobar.down.sql")
-	createMigrationFile(t, pkg, "/7_foobar.up.sql")
-	createMigrationFile(t, pkg, "/7_foobar.down.sql")
-
-	return &Pkger{
-		Pkger: pkg,
-	}
-}
-
-func createMigrationFile(t *testing.T, pkg pkging.Pkger, m string) {
-	_, err := pkg.Create(m)
-	if err != nil {
-		t.Fatalf("failed to package migration file %q: %v\n", m, err)
-	}
+	return pkg, nil
 }
