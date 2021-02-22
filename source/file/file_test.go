5a20f79920d236ceed3b01e91beb04d47a9a20b4
Attila Rickert <attila.rickert@z-consulting.eu>
2021-02-22 15:31:37 +0100

    fix module in file migration source

--- source/file/file_test.go
+++ source/file/file_test.go
@@ -9,7 +9,7 @@ import (
 	"path/filepath"
 	"testing"
 
-	st "github.com/golang-migrate/migrate/v4/source/testing"
+	st "github.com/nagyistzcons/migrate/v4/source/testing"
 )
 
 func Test(t *testing.T) {
