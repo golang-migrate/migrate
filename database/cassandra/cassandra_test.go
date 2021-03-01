eceb2e8dabe8765a448078cf9599a91cde370d24
Cynthia <cynthia@cynthia.dev>
2021-03-01 18:44:24 +0100

    chore: use an optimized schema for Cassandra
    Also added the mention of Scylla on the README
    
    fix: #455

--- database/cassandra/cassandra_test.go
+++ database/cassandra/cassandra_test.go
@@ -3,19 +3,16 @@ package cassandra
 import (
 	"context"
 	"fmt"
-	"github.com/golang-migrate/migrate/v4"
 	"strconv"
 	"testing"
-)
 
-import (
 	"github.com/dhui/dktest"
 	"github.com/gocql/gocql"
-)
+	"github.com/golang-migrate/migrate/v4"
 
-import (
 	dt "github.com/golang-migrate/migrate/v4/database/testing"
 	"github.com/golang-migrate/migrate/v4/dktesting"
+
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
@@ -52,7 +49,7 @@ func isReady(ctx context.Context, c dktest.ContainerInfo) bool {
 	}
 	defer p.Close()
 	// Create keyspace for tests
-	if err = p.Query("CREATE KEYSPACE testks WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor':1}").Exec(); err != nil {
+	if err = p.Query("CREATE KEYSPACE testks WITH REPLICATION = {'class': 'SimpleStrategy', 'replication_factor': 1}").Exec(); err != nil {
 		return false
 	}
 	return true
@@ -102,5 +99,71 @@ func TestMigrate(t *testing.T) {
 			t.Fatal(err)
 		}
 		dt.TestMigrate(t, m)
+	})
+}
+
+func TestSchemaVersionV2Migration(t *testing.T) {
+	dktesting.ParallelTest(t, specs, func(t *testing.T, c dktest.ContainerInfo) {
+		ip, portStr, err := c.Port(9042)
+		if err != nil {
+			t.Fatal("Unable to get mapped port:", err)
+		}
+
+		port, err := strconv.Atoi(portStr)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		addr := fmt.Sprintf("cassandra://%v:%v/testks", ip, portStr)
+
+		// Setup fake legacy version table
+		cluster := gocql.NewCluster(ip)
+		cluster.Port = port
+		cluster.Keyspace = "testks"
+		cluster.Consistency = gocql.All
+		sess, err := cluster.CreateSession()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		defer sess.Close()
+		if err = sess.Query("CREATE TABLE schema_migrations (version bigint, dirty boolean, PRIMARY KEY(version))").Exec(); err != nil {
+			t.Fatal(err)
+		}
+		if err = sess.Query("INSERT INTO schema_migrations (version, dirty) VALUES (20210301171700, false)").Exec(); err != nil {
+			t.Fatal(err)
+		}
+
+		// Init driver
+		p := &Cassandra{}
+		d, err := p.Open(addr)
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer func() {
+			if err := d.Close(); err != nil {
+				t.Error(err)
+			}
+		}()
+
+		// Migration should have occurred; we need to check that the `dummy` field now exists, and that data has been preserved.
+		var count int8
+		err = sess.Query("SELECT COUNT(*) FROM system_schema.columns WHERE keyspace_name = 'testks' AND table_name = 'schema_migrations' AND column_name = 'dummy';").Scan(&count)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if count != 1 {
+			t.Error("Expected column dummy to be present in the schema_migrations table")
+		}
+
+		ver, dirty, err := d.Version()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if ver != 20210301171700 || dirty {
+			t.Error("Expected data to be preserved after migration")
+		}
 	})
 }
