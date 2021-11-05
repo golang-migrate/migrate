4ba43183fd2a5b76de443531f0e6f4f28d69dbdf
Steve Ramage <commits@sjrx.net>
2021-11-04 21:45:28 -0700

    Lint fixes again #647

--- database/mongodb/mongodb.go
+++ database/mongodb/mongodb.go
@@ -36,7 +36,7 @@ const LockIndexName = "lock_unique_key"                  // the name of the inde
 const contextWaitTimeout = 5 * time.Second               // how long to wait for the request to mongo to block/wait for.
 
 var (
-	ErrNoDatabaseName        = fmt.Errorf("no database name")
+	ErrNoDatabaseName            = fmt.Errorf("no database name")
 	ErrNilConfig                 = fmt.Errorf("no config")
 	ErrLockTimeoutConfigConflict = fmt.Errorf("both x-advisory-lock-timeout-interval and x-advisory-lock-timout-interval were specified")
 )
