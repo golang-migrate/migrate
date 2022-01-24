380aa3279de865686dcbb170ca1c608062aa3dd9
David Boon <david.boon@gmail.com>
2022-01-24 12:28:44 -0800

    replace multi statement parsing with comment aware parsing

--- database/multistmt/parse.go
+++ database/multistmt/parse.go
@@ -2,8 +2,8 @@
 package multistmt
 
 import (
-	"bufio"
 	"bytes"
+	"fmt"
 	"io"
 )
 
@@ -33,14 +33,55 @@ func splitWithDelimiter(delimiter []byte) func(d []byte, atEOF bool) (int, []byt
 
 // Parse parses the given multi-statement migration
 func Parse(reader io.Reader, delimiter []byte, maxMigrationSize int, h Handler) error {
-	scanner := bufio.NewScanner(reader)
-	scanner.Buffer(make([]byte, 0, StartBufSize), maxMigrationSize)
-	scanner.Split(splitWithDelimiter(delimiter))
-	for scanner.Scan() {
-		cont := h(scanner.Bytes())
-		if !cont {
+	var err error = nil
+	// buf is the bytes read from input reader
+	buf := make([]byte, 4096)
+	// true when we're ignoring input(during comments)
+	discard := false
+	// accumulate statements intermediate buffer, this buffer will be incomplete
+	// until end-of-statement char ';'
+	accum := make([]byte, 0, 1024)
+	// completed statements, contents of accum will be dumped in here
+	stmts := make([][]byte, 0, 2048)
+	for err == nil {
+		n, err := reader.Read(buf)
+		if n > 0 {
+			for i := range buf[:n] {
+				if len(buf) > 2 &&  buf[i] == '-' && buf[i+1] == '-' && buf[i+2] == '-' {
+					discard = true
+				}
+				fmt.Printf("%c", buf[i])
+				switch ch := buf[i]; ch {
+				case ';':
+					if !discard {
+						// include ';' in accum
+						accum = append(accum, ch)
+						stmts = append(stmts, accum)
+						// reset accum, maintain allocated memory
+						accum = accum[:0]
+					}
+				case '\n':
+					// at end of line, reset discard
+					discard = false
+				default:
+					if !discard {
+						accum = append(accum, ch)
+					}
+				}
+			}
+		}
+
+		if err == io.EOF {
 			break
 		}
+
+		if err != nil {
+			return err
+		}
 	}
-	return scanner.Err()
+
+	for _, stmt := range stmts {
+		h(stmt)
+	}
+	return nil
 }
