ab05af29a8f0d8ec90591e4c49b8b354654335c1
Fabio Marini <fabio.marini@gninformatica.com>
2022-11-14 17:44:42 +0100

    feat: added support for Neo4j 5.x migrations

--- database/neo4j/neo4j_test.go
+++ database/neo4j/neo4j_test.go
@@ -20,10 +20,12 @@ var (
 	opts = dktest.Options{PortRequired: true, ReadyFunc: isReady,
 		Env: map[string]string{"NEO4J_AUTH": "neo4j/migratetest", "NEO4J_ACCEPT_LICENSE_AGREEMENT": "yes"}}
 	specs = []dktesting.ContainerSpec{
-		{ImageName: "neo4j:4.0", Options: opts},
-		{ImageName: "neo4j:4.0-enterprise", Options: opts},
-		{ImageName: "neo4j:3.5", Options: opts},
-		{ImageName: "neo4j:3.5-enterprise", Options: opts},
+		{ImageName: "neo4j:5-community", Options: opts},
+		//{ImageName: "neo4j:5-enterprise", Options: opts},
+		{ImageName: "neo4j:4.4-community", Options: opts},
+		//{ImageName: "neo4j:4.4-enterprise", Options: opts},
+		//{ImageName: "neo4j:3.5-community", Options: opts},
+		//{ImageName: "neo4j:3.5-enterprise", Options: opts},
 	}
 )
 
