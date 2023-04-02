3449d1bc687cd53df79a0d2da4f5c3c2398afeed
Joseph Watson <jtwatson@linux-consulting.us>
2023-04-02 02:00:26 -0400

    fix(spanner): Fix issue where a provided spanner client is closed by Migrate.Close()

--- database/spanner/spanner.go
+++ database/spanner/spanner.go
@@ -63,14 +63,16 @@ type Spanner struct {
 }
 
 type DB struct {
-	admin *sdb.DatabaseAdminClient
-	data  *spanner.Client
+	admin  *sdb.DatabaseAdminClient
+	data   *spanner.Client
+	shared bool
 }
 
 func NewDB(admin sdb.DatabaseAdminClient, data spanner.Client) *DB {
 	return &DB{
-		admin: &admin,
-		data:  &data,
+		admin:  &admin,
+		data:   &data,
+		shared: true,
 	}
 }
 
@@ -140,6 +142,9 @@ func (s *Spanner) Open(url string) (database.Driver, error) {
 
 // Close implements database.Driver
 func (s *Spanner) Close() error {
+	if s.db.shared {
+		return nil
+	}
 	s.db.data.Close()
 	return s.db.admin.Close()
 }
