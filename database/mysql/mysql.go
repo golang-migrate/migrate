8ddcf73022723837e0b034d2dccdf3c61ef0e48e
Kemal Kalandarov <kemalkalandarov@gmail.com>
2024-05-05 15:20:18 +0300

    removed tx level serializable

--- database/mysql/mysql.go
+++ database/mysql/mysql.go
@@ -356,7 +356,7 @@ func (m *Mysql) Run(migration io.Reader) error {
 }
 
 func (m *Mysql) SetVersion(version int, dirty bool) error {
-	tx, err := m.conn.BeginTx(context.Background(), &sql.TxOptions{Isolation: sql.LevelSerializable})
+	tx, err := m.conn.BeginTx(context.Background(), &sql.TxOptions{})
 	if err != nil {
 		return &database.Error{OrigErr: err, Err: "transaction start failed"}
 	}
