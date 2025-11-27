8debcd4b83a2bee62358726b55cd4c924c307cc0
Timo Riski <github@timoriski.fi>
2025-11-27 14:50:18 +0200

    duckdb: align tests with sqlite

--- database/duckdb/duckdb_test.go
+++ database/duckdb/duckdb_test.go
@@ -1,9 +1,12 @@
 package duckdb
 
 import (
+	"database/sql"
 	"fmt"
 	"path/filepath"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 
 	_ "github.com/duckdb/duckdb-go/v2"
 	"github.com/golang-migrate/migrate/v4"
@@ -28,24 +31,66 @@ func Test(t *testing.T) {
 func TestMigrate(t *testing.T) {
 	dir := t.TempDir()
 	dbFile := filepath.Join(dir, "test.duckdb")
-	addr := fmt.Sprintf("duckdb://%s", dbFile)
 
-	ddb := &DuckDB{}
-	d, err := ddb.Open(addr)
+	db, err := sql.Open("duckdb", dbFile)
 	if err != nil {
-		t.Fatalf("calling Open() on addr %s: %s", addr, err)
+		t.Fatalf("sql open: %s", err)
+	}
+	defer func() {
+		assert.NoError(t, db.Close())
+	}()
+
+	driver, err := WithInstance(db, &Config{})
+	if err != nil {
+		t.Fatalf("with instance: %s", err)
 	}
 
 	m, err := migrate.NewWithDatabaseInstance(
 		"file://./examples/migrations",
 		"main",
-		d,
-	)
+		driver)
 	if err != nil {
 		t.Fatalf("new migrate: %s", err)
 	}
 
 	dt.TestMigrate(t, m)
+}
+
+func TestMigrationTable(t *testing.T) {
+	dir := t.TempDir()
+	dbFile := filepath.Join(dir, "test.duckdb")
+
+	db, err := sql.Open("duckdb", dbFile)
+	if err != nil {
+		t.Fatalf("sql open: %s", err)
+	}
+	defer func() {
+		assert.NoError(t, db.Close())
+	}()
+
+	config := &Config{
+		MigrationsTable: "custom_migrations",
+	}
+	driver, err := WithInstance(db, config)
+	if err != nil {
+		t.Fatalf("with instance: %s", err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://./examples/migrations",
+		"main",
+		driver)
+	if err != nil {
+		t.Fatalf("new migrate: %s", err)
+	}
+
+	if err := m.Up(); err != nil {
+		t.Fatalf("up: %s", err)
+	}
+
+	if _, err := db.Query(fmt.Sprintf("SELECT * FROM %s", config.MigrationsTable)); err != nil {
+		t.Fatalf("query migrations table: %s", err)
+	}
 }
 
 func TestNoTxWrap(t *testing.T) {
@@ -69,7 +114,8 @@ func TestNoTxWrapInvalidValue(t *testing.T) {
 
 	ddb := &DuckDB{}
 	_, err := ddb.Open(addr)
-	if err == nil {
-		t.Fatal("expected error for invalid x-no-tx-wrap value")
+	if assert.Error(t, err) {
+		assert.Contains(t, err.Error(), "x-no-tx-wrap")
+		assert.Contains(t, err.Error(), "invalid syntax")
 	}
 }
