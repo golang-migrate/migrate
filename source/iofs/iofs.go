c7ff14f1e7bb17fe6a80587f20fe308dfb4f9da0
Pascal Bourdier <pascal.bourdier@gmail.com>
2025-12-01 20:30:05 +0100

    fix tests

--- source/iofs/iofs.go
+++ source/iofs/iofs.go
@@ -29,7 +29,7 @@ func New(fsys fs.FS, path string) (source.Driver, error) {
 // Open is part of source.Driver interface implementation.
 // Open cannot be called on the iofs passthrough driver.
 func (d *driver) Open(url string) (source.Driver, error) {
-	return nil, errors.New("Open() cannot be called on the iofs passthrough driver")
+	return nil, errors.New("open() cannot be called on the iofs passthrough driver")
 }
 
 // PartialDriver is a helper service for creating new source drivers working with
